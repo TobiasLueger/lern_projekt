16d11f15d54255808d78e1f299a2de3d336bdd7e
Tobias Lüger <tobias.lueger@brandung.de>
2018-12-05 14:50:34 +0100

    designed better gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,53 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	watch = require('gulp-watch');
+// import babel from 'gulp-babel';
+// import concat from 'gulp-concat';
+const del = require('del');
+const gulp = require('gulp');
+// import uglify from 'gulp-uglify';
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
 
 
-gulp.task('default', function()
-{
-	gulp.watch('scss/*.scss', function()
-		{
-			return gulp.src('scss/main.scss')
-				.pipe(sass())
-				.pipe(gulp.dest('public/css'));
-		}
-	);
+const server = browserSync.create();
+
+const paths = {
+	scripts: {
+		html: './*.html',
+		js: 'js/*.js',
+		scss_main: 'scss/*.scss',
+		scss_partials: 'scss/partials/*.scss',
+		dest: 'public/css/'
+	}
+};
+
+const clean = () => del(['dist']);
+
+function scripts() {
+	return gulp.src(paths.scripts.js && paths.scripts.scss_main, { sourcemaps: true })
+		// .pipe(uglify())
+		// .pipe(babel())
+		.pipe(sass())
+		// .pipe(concat('index.min.js'))
+		.pipe(gulp.dest(paths.scripts.dest));
 }
-);
+
+function reload(done) {
+	server.reload();
+	done();
+}
+
+function serve(done) {
+	server.init({
+		server: {
+			baseDir: './'
+		}
+	});
+	done();
+}
+
+const watch = () => gulp.watch(paths.scripts.js && paths.scripts.scss_main && paths.scripts.scss_partials && paths.scripts.html, gulp.series(scripts, reload));
+
+gulp.task('default', gulp.series(clean, scripts, serve, watch),);
+
+
+
+
