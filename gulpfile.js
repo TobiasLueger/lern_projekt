ee4430715d5c88fe48a46f5c09d4c20f82bb3854
Tobias Lüger <tobias.lueger@brandung.de>
2018-12-06 11:31:43 +0100

    add compiling js funktion  in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-// import babel from 'gulp-babel';
-// import concat from 'gulp-concat';
+const babel = require('gulp-babel');
+// const concat = require('gulp-concat');
+// const rename = require('gulp-rename');
 const del = require('del');
 const gulp = require('gulp');
-// import uglify from 'gulp-uglify';
+const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 
@@ -11,24 +12,40 @@ const server = browserSync.create();
 
 const paths = {
 	scripts: {
-		html: './*.html',
 		js_main: 'js/*.js',
-		js_partials: 'js/partials/*.js',
+		js_partials: 'js/partials/*.js'
+	},
+
+	styles: {
 		scss_main: 'scss/*.scss',
-		scss_partials: 'scss/partials/*.scss',
-		dest: 'public/css/'
-	}
+		scss_partials: 'scss/partials/*.scss'
+	},
+
+	templates: {
+		html: './*.html'
+	},
+
+	expo: {
+		dest_css: 'public/css/',
+		dest_js: 'public/js/'
+	},
 };
 
 const clean = () => del(['dist']);
 
-function scripts() {
-	return gulp.src(paths.scripts.scss_main, { sourcemaps: true })
-		// .pipe(uglify())
-		// .pipe(babel())
+function styles() {
+	return gulp.src(paths.styles.scss_main, { sourcemaps: true })
 		.pipe(sass())
-		// .pipe(concat('index.min.js'))
-		.pipe(gulp.dest(paths.scripts.dest));
+		.pipe(gulp.dest(paths.expo.dest_css));
+}
+
+function scripts() {
+	return gulp.src(paths.scripts.js_main, { sourcemaps: true })
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.expo.dest_js));
 }
 
 function reload(done) {
@@ -45,10 +62,9 @@ function serve(done) {
 	done();
 }
 
-const watch = () => gulp.watch(paths.scripts.js_main && paths.scripts.js_partials && paths.scripts.scss_main && paths.scripts.scss_partials && paths.scripts.html, gulp.series(scripts, reload));
+const watch = () => gulp.watch(paths.scripts.js_main && paths.scripts.js_partials && paths.styles.scss_main && paths.styles.scss_partials && paths.templates.html, gulp.series(styles, scripts, reload));
 
-gulp.task('default', gulp.series(clean, scripts, serve, watch),);
-
+gulp.task('default', gulp.series(clean, scripts, styles, serve, watch),);
 
 
 
