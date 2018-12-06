bd91d083579ebede24968cb48bd5251f552c4225
Tobias Lüger <tobias.lueger@brandung.de>
2018-12-06 15:40:50 +0100

    added readme file

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
+[![npm Version][NPM VERSION BADGE]][NPM PAGE]
+[![Node.js][NODE VERSION BADGE]][NODE PAGE]
+[![gulp][GULP VERSION BADGE]][GULP PAGE]
+
+
 # lern_projekt
-Simple repo to test my skills!
+*Simple repo to learn new things!*
+
+## What have I done?
+
+- [Gulp](https://gulpjs.com/): I have build my own workflow with gulp and npm scripts.
+- [SCSS](https://sass-lang.com/): I write my CSS3 code in SCSS and it is compiled over 'gulp' to one minified CSS file.
+
+
+## Documentation
+
+**Install all from package.json**
+
+```
+$ npm install
+```
+
+**Get started**
+
+```
+$ npm start
+```
+
+
+[NODE PAGE]: https://nodejs.org/
+[NODE VERSION BADGE]: https://img.shields.io/badge/node-v8.11.3-orange.svg
+[NPM PAGE]: https://www.npmjs.com/
+[NPM VERSION BADGE]: https://img.shields.io/badge/npm-6.4.1-green.svg
+[GULP PAGE]: https://gulpjs.com/
+[GULP VERSION BADGE]: https://img.shields.io/badge/gulp-4.0.0-blue.svg
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 const babel = require('gulp-babel');
-// const concat = require('gulp-concat');
-// const rename = require('gulp-rename');
 const del = require('del');
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
+const uglifycss = require('gulp-uglifycss');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
-
-
 const server = browserSync.create();
 
 const paths = {
@@ -36,6 +33,10 @@ const clean = () => del(['dist']);
 function styles() {
 	return gulp.src(paths.styles.scss_main, { sourcemaps: true })
 		.pipe(sass())
+		.pipe(uglifycss({
+			"maxLineLen": 80,
+			"uglyComments": true
+		}))
 		.pipe(gulp.dest(paths.expo.dest_css));
 }
 
@@ -62,9 +63,20 @@ function serve(done) {
 	done();
 }
 
+function buildDirectory() {
+	return gulp.src('*.*', {read: false})
+		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(gulp.dest('./dist/js'))
+		.pipe(gulp.dest('./dist/img'));
+};
+
 const watch = () => gulp.watch(paths.scripts.js_main && paths.scripts.js_partials && paths.styles.scss_main && paths.styles.scss_partials && paths.templates.html, gulp.series(styles, scripts, reload));
 
 gulp.task('default', gulp.series(clean, scripts, styles, serve, watch),);
+
+
+gulp.task('build', gulp.series(clean, buildDirectory),);
 
 
 
